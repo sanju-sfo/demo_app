6822b58d1394bb790072bdeae5dfaa58b8fb5e85
sanjsark <sanjsark@Sanjeev-Sarkars-MacBook-Pro.local>
2012-09-18 16:12:23 -0700

    Validation corrected

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,6 @@ class Micropost < ActiveRecord::Base
     attr_accessible :content, :user_id
     belongs_to :user
 
-    validates :content , length: { maximum: 2}
+    validates :content , length: { maximum: 100}
 
 end
